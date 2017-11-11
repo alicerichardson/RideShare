4eebe43077ce01d8650802d1bc83fcd8e8384afe
adaschnake <adaschnake@n244-33.mtholyoke.edu>
2017-11-11 17:20:28 -0500

    Added Post and Request activities, moving between activities

--- app/src/main/java/com/example/alicerichardson/rideshare/MainActivity.java
+++ app/src/main/java/com/example/alicerichardson/rideshare/MainActivity.java
@@ -1,10 +1,48 @@
 package com.example.alicerichardson.rideshare;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by alicerichardson on 11/11/17.
  */
 
 public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button hostingButton = (Button) findViewById(R.id.hosting_button);
+        Button lookingButton = (Button) findViewById(R.id.looking_button);
+
+
+        hostingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                post();
+                           }
+        });
+
+        lookingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                request();
+                            }
+        });
+    }
+
+    public void post()
+    {
+        startActivity(new Intent(this, PostActivity.class));
+    }
+
+    public void request()
+    {
+        startActivity(new Intent(this, RequestActivity.class));
+    }
 }
---
+++ app/src/main/java/com/example/alicerichardson/rideshare/PostActivity.java
@@ -0,0 +1,42 @@
+package com.example.alicerichardson.rideshare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+/**
+ * Created by adaschnake on 11/11/17.
+ */
+
+
+public class PostActivity extends AppCompatActivity {
+
+    Spinner toSpinner;
+    Spinner fromSpinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+        populateSpinner(R.id.to_spinner);
+        populateSpinner(R.id.from_spinner);
+
+        fromSpinner = (Spinner) findViewById(R.id.from_spinner);
+        toSpinner = (Spinner) findViewById(R.id.to_spinner);
+    }
+
+    private void populateSpinner(int newSpinID) {
+        Spinner spinner = (Spinner) findViewById(newSpinID);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.locations_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+
+}
