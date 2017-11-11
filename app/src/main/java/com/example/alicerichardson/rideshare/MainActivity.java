6198a2a87a32491398e61988919392e2709ab34d
Alice Richardson <alicerichardson@n245-77.mtholyoke.edu>
2017-11-11 15:43:46 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alicerichardson/rideshare/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.alicerichardson.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alicerichardson on 11/11/17.
+ */
+
+public class MainActivity extends AppCompatActivity{
+}
