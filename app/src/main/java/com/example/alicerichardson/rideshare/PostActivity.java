cf57d95c24f729ea192314adde3345c6a9676c26
adaschnake <adaschnake@n244-33.mtholyoke.edu>
2017-11-11 18:35:32 -0500

    Edits to Ride and Request activities

--- app/src/main/java/com/example/alicerichardson/rideshare/PostActivity.java
+++ app/src/main/java/com/example/alicerichardson/rideshare/PostActivity.java
@@ -2,8 +2,18 @@ package com.example.alicerichardson.rideshare;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Date;
 
 /**
  * Created by adaschnake on 11/11/17.
@@ -14,7 +24,17 @@ public class PostActivity extends AppCompatActivity {
 
     Spinner toSpinner;
     Spinner fromSpinner;
-
+    String[] results;
+    EditText seats;
+    CheckBox pets;
+    CheckBox luggage;
+    CheckBox smoking;
+    CheckBox food;
+    CheckBox money;
+    HashMap ridesMap;
+    DatePicker datePicker;
+    TimePicker timePicker;
+    EditText timeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +46,25 @@ public class PostActivity extends AppCompatActivity {
 
         fromSpinner = (Spinner) findViewById(R.id.from_spinner);
         toSpinner = (Spinner) findViewById(R.id.to_spinner);
+        Button goButton = (Button) findViewById(R.id.go_button);
+        seats = (EditText) findViewById(R.id.seatsText);
+        pets = (CheckBox) findViewById(R.id.petBox);
+        luggage = (CheckBox) findViewById(R.id.luggageBox);
+        smoking = (CheckBox) findViewById(R.id.smokingBox);
+        food = (CheckBox) findViewById(R.id.foodBox);
+        money = (CheckBox) findViewById(R.id.moneyBox);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timeText = (EditText) findViewById(R.id.time_text);
+
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createNewRide();
+            }
+        });
     }
 
     private void populateSpinner(int newSpinID) {
@@ -38,4 +77,45 @@ public class PostActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
     }
 
+<<<<<<< Updated upstream
+=======
+    public void createNewRide()
+    {
+        //get info from spinners, date/time, check boxes
+        results = getSpinnerResultsArray();
+        int numSeats = Integer.parseInt(seats.getText().toString());
+        int time = timeText.getText().toString();
+        //create new ride and add to
+        ridesMap.put(results[1], new Ride(results[1], results[0], time, getDate(), numSeats, getCheckBoxResultsArray()));
+        //return to home screen?
+        //or success message
+    }
+
+    private String[] getSpinnerResultsArray()
+    {
+        //put the results from the spinners into an array
+        String[] results =  {fromSpinner.getSelectedItem().toString(), toSpinner.getSelectedItem().toString()};
+        return results;
+    }
+
+    private boolean[] getCheckBoxResultsArray()
+    {
+        boolean[] results = {pets.isChecked(), luggage.isChecked(), smoking.isChecked(), food.isChecked(), money.isChecked()};
+        return results;
+    }
+
+    private Date getDate()
+    {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = dateFormat.format(new Date(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth()));
+        return new Date(date);
+    }
+
+//    private String getTime()
+//    {
+//        return timePicker.getHour() + ";" + timePicker.getMinute();
+//    }
+
+
+>>>>>>> Stashed changes
 }
